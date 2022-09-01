ea9f358c0400c0eb71305c0c617761fb3387c8c6
baripembo <erikawei@gmail.com>
2022-09-01 15:24:11 -0400

    updating benchmarking charts

--- src/pages/OCHACharts.svelte
+++ src/pages/OCHACharts.svelte
@@ -10,10 +10,15 @@
 
 
   let ochaData = [];
+  let ochaContent = [];
   let serviceData = [];
-  let fullExtents;
+  let serviceContent = [];
+  let fullExtents, fullContentExtents;
 
-  const dataURL = 'https://proxy.hxlstandard.org/data.csv?dest=data_edit&strip-headers=on&tagger-match-all=on&tagger-05-header=service+name+and+url&tagger-05-tag=%23service%2Bname&tagger-06-header=date+(mm%2Fdd%2Fyyyy)&tagger-06-tag=%23date&tagger-08-header=users&tagger-08-tag=%23users&header-row=1&url=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vR8h_9uY_SCu9FTZr6Mq5htrUAF7eWXdme-rUsVbWgX9cF2hTvkmrWTisMmKoDAR462SJbDh9FIzwSf%2Fpub%3Fgid%3D1837949461%26single%3Dtrue%26output%3Dcsv';
+  const dataURL = 'https://proxy.hxlstandard.org/data.csv?dest=data_edit&strip-headers=on&force=on&tagger-match-all=on&tagger-05-header=service+name+and+url&tagger-05-tag=%23service%2Bname&tagger-06-header=date+(mm%2Fdd%2Fyyyy)&tagger-06-tag=%23date&tagger-08-header=users&tagger-08-tag=%23users&tagger-17-header=url&tagger-17-tag=%23url&header-row=1&url=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vR8h_9uY_SCu9FTZr6Mq5htrUAF7eWXdme-rUsVbWgX9cF2hTvkmrWTisMmKoDAR462SJbDh9FIzwSf%2Fpub%3Fgid%3D1837949461%26single%3Dtrue%26output%3Dcsv';
+
+
+  const contentURL = 'https://proxy.hxlstandard.org/data.csv?dest=data_edit&strip-headers=on&force=on&tagger-match-all=on&tagger-05-header=service+name+and+url&tagger-05-tag=%23service%2Bname&tagger-06-header=date+(mm%2Fdd%2Fyyyy)&tagger-06-tag=%23date&tagger-08-header=users&tagger-08-tag=%23users&tagger-17-header=url&tagger-17-tag=%23url&header-row=1&url=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vR8h_9uY_SCu9FTZr6Mq5htrUAF7eWXdme-rUsVbWgX9cF2hTvkmrWTisMmKoDAR462SJbDh9FIzwSf%2Fpub%3Fgid%3D1204537761%26single%3Dtrue%26output%3Dcsv';
 
   const extentGetters = {
     x: d => d['#date'],
@@ -21,6 +26,7 @@
   };
 
   onMount(async () => {
+    //data sites
     ochaData = await csv(dataURL, function(data) {
       data['#users'] = +data['#users'];
       data['#date'] = new Date(data['#date']);
@@ -34,8 +40,11 @@
 
     //group data by service
     serviceData = flatGroup(ochaData, d => d['#service+name']);
-
-    $: console.log('2',serviceData)
+    
+    //find max val by service
+    serviceData.forEach(function(service) {
+      service.push(max(service[1], d => d['#users']))
+    });
     
     //find max val by service
     serviceData.forEach(function(service) {
@@ -44,12 +53,37 @@
 
     //sort by high to low val
     serviceData.sort((a, b) => b[2] - a[2]);
+
+
+    //content sites
+    ochaContent = await csv(contentURL, function(data) {
+      data['#users'] = +data['#users'].replace(/,/g, '');
+      data['#date'] = new Date(data['#date']);
+      return data;
+    }).then((data) => {
+      return data;
+    });
+
+    //calculate scale for all small multiples
+    fullContentExtents = calcExtents(ochaContent, extentGetters);
+    
+    //group data by service
+    serviceContent = flatGroup(ochaContent, d => d['#service+name']);
+    
+    //find max val by service
+    serviceContent.forEach(function(service) {
+      service.push(max(service[1], d => d['#users']))
+    });
+    
+    //sort by high to low val
+    serviceContent.sort((a, b) => b[2] - a[2]);
   });
 </script>
 
 
 <h3>User Growth in OCHA Platforms (2018 – 2022)</h3>
 <div class='group-chart-container'>
+  <h4>Data Sites</h4>
   {#each serviceData as service}
     <div class='multiples-chart-container'>
       <LayerCake
@@ -63,7 +97,42 @@
         <Html>
           <div class='chart-header'>
             <h5>{service[0]}</h5>
-            <p>url goes here</p>
+            <p>{service[1][0]['#url']}</p>
+          </div>
+        </Html>
+        <Svg>
+          <AxisX
+            baseline={true}
+            formatTick={timeFormat('%Y')}
+            gridlines={false}
+            snapTicks={true}
+            ticks={fullExtents.x}
+          />
+          <AxisY
+            formatTick={format('.2s')}
+            ticks={fullExtents.y}
+          />
+          <Line stroke='#1EBFB3' />
+        </Svg>
+      </LayerCake>
+    </div>
+  {/each}
+
+  <h4>Content Sites</h4>
+  {#each serviceContent as service}
+    <div class='multiples-chart-container'>
+      <LayerCake
+        padding={{ top: 30, right: 20, bottom: 0, left: 20 }}
+        data={ service[1] }
+        x='#date'
+        y='#users'
+        xScale={ scaleTime() }
+        yDomain={fullExtents.y}
+      >
+        <Html>
+          <div class='chart-header'>
+            <h5>{service[0]}</h5>
+            <p>{service[1][0]['#url']}</p>
           </div>
         </Html>
         <Svg>
@@ -89,5 +158,9 @@
 <style>
   .chart-header {
     display: block;
+    text-align: center;
+  }
+  h4 {
+    font-size: 14px;
   }
 </style>
